468689c4c7aa6957c2a76156aea80d2aedbe4756
takahirom <takam.dev@gmail.com>
2025-05-14 10:27:10 +0900

    Remove unneeded dependencies

--- skroll/build.gradle.kts
+++ skroll/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 }
 
 dependencies {
-  // Apply the kotlinx bundle of dependencies from the version catalog (`gradle/libs.versions.toml`).
-  implementation(libs.bundles.kotlinxEcosystem)
   testImplementation(kotlin("test"))
+  implementation(libs.kotlinxCoroutines)
 }
