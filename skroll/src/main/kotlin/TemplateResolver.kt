a5a50194bee69770c39d672e5a27cfb2ff87c3a1
takahirom <takam.dev@gmail.com>
2025-05-14 13:47:19 +0900

    Rename template embedding

--- skroll/src/main/kotlin/TemplateResolver.kt
+++ skroll/src/main/kotlin/TemplateResolver.kt
@@ -22,7 +22,7 @@ class SimpleTemplateResolver : TemplateResolver {
         parameters.forEach { (key, value) ->
             // Basic placeholder replacement.
             // Consider proper escaping for shell commands, JSON, etc., in a real implementation.
-            resolvedCommand = resolvedCommand.replace("{$key}", value)
+            resolvedCommand = resolvedCommand.replace("{{$key}}", value)
         }
         return resolvedCommand
     }
